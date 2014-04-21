207159d8a35b6131ae77c7a1d9c57588d13c833c
Austin Riendeau <austin@apriendeau.com>
2014-04-21 11:42:32 -0600

    removes ip

--- public/config.js
+++ public/config.js
@@ -21,7 +21,7 @@ function (Settings) {
      * kibana installed on. You probably want to set it to the FQDN of your
      * elasticsearch host
      */
-    elasticsearch: "http://107.170.234.233:9200",
+    elasticsearch: "http://127.0.0.1:9200",
 
     /** @scratch /configuration/config.js/5
      * ==== default_route
